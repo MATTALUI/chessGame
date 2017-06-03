7d88ece625da37fe0aeec03997056afd52ffe183
Matthew Hummer <biggreyson@gmail.com>
2017-06-03 15:23:05 -0600

    fix piece/square selection; comment out unnecessary functions; make locally playable

--- chess.js
+++ chess.js
@@ -1,121 +1,91 @@
 $(document).ready(()=>{
   generateBoard();
   $('.square').css('height', $('#a1').css('width'));
-  setPieces();
-  $('#boardZone').on('click','.piece', selectPiece);
-  $('#boardZone').on('click','.square', selectNewLocation);
+  $('#boardZone').on('click','.piece', handlePieceClick);
+  $('#boardZone').on('click','.square', handleSquareClick);
+  $('#newGame').on('click', ()=>{
+    $.ajax({
+    url: 'http://54.241.184.191/board/0',
+    type: 'DELETE',
+    success: function(result) {
+        setGameState(result);
+      }
+    });
+  });
+  $.get('http://54.241.184.191/board/0', (data)=>{
+    setGameState(data);
+  })
+
 });
 $(window).resize(()=>{
     $('.square').css('height', $('#a1').css('width'));
-
 });
 
-function generateBoard(){
-  const $body = $('body');
-  let alphaIndex = ['a','b','c','d','e','f','g','h'];
-  let color;
-  for(let ni=8; ni>=1;ni--){
 
-    for(let ai = 0; ai<8;ai++){
+function movePiece(from, to){
+  let move = {
+    'to': to,
+    'from': from
+  }
+  $.ajax({
+    type: "POST",
+    url: "http://54.241.184.191/board/0",
+    data: JSON.stringify(move),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+      console.log(move);
+      setGameState(data);
+      $('.active').removeClass('active');
+    },
+    failure: function(errMsg) {
+        console.log('meow');
+    },
+    error: function(response){
+      console.log('ruff');
+      console.log(response);
+    }});
 
-      if(ni%2==0&&ai%2==0){
-        color = 'light';
-      }else if (ni%2==1&&ai%2==1){
-        color = 'light';
-      }else{
-        color = 'dark'
-      }
-
-
-      $('#boardZone').append(`<div class="square ${color}" id="${alphaIndex[ai]}${ni}"></div>`);
+}
+function handlePieceClick(){
+  let squareOn = $(event.target).parent();
+  let isThereAnActiveSquare = ($('.active').length !== 0);
+  if ($(event.target).parent().hasClass('active')){
+    $(squareOn).toggleClass('active');
+    return;
+  }
+  if(isThereAnActiveSquare){
+    let selectedTeam = $('.active').children()[0].dataset.team;
+    if(event.target.dataset.team != selectedTeam){
+      let from = $('.active').attr('id')
+      let to = $(event.target).parent().attr('id')
+      movePiece(from,to);
+    }else{
+      $('.active').toggleClass('active');
+      $(squareOn).toggleClass('active');
     }
+  }else{
+    $('.active').toggleClass('active');
+    $(squareOn).toggleClass('active');
+  }
 
+}
+function handleSquareClick(){
+  let $squareClicked = $(event.path.reverse()[5]);
+  let isThereAnActiveSquare = ($('.active').length !== 0);
+  if ($squareClicked.children().length === 0 && isThereAnActiveSquare){
+    let to = event.target.id;
+    let from = $('.active').attr('id');
+    movePiece(from, to);
   }
 }
 function addPiece(pieceType, color, location){
   $(`#${location}`).html(`<img src="pieces/${color}${pieceType}.png" class="piece" data-team="${color}" data-piece="${pieceType}">`);
 }
-function removePiece(location){
-  $(`#${location}`).html('')
-}
-function movePiece(from, to){
-  $('.active').removeClass('active');
-  $(`#${to}`).html($(`#${from}`).html());
-  $(`#${from}`).html('');
-}
-function setPieces(){
-  for(let i=0; i<8;i++){
-    let letters = ['a','b','c','d','e','f','g','h'];
-    addPiece('pawn','black',`${letters[i]}7`);
-    addPiece('pawn','white',`${letters[i]}2`);
-  }
-  addPiece('rook', 'white', 'a1');
-  addPiece('rook', 'white', 'h1');
-  addPiece('rook', 'black', 'a8');
-  addPiece('rook', 'black', 'h8');
-  addPiece('knight', 'white', 'b1');
-  addPiece('knight', 'white', 'g1');
-  addPiece('knight', 'black', 'b8');
-  addPiece('knight', 'black', 'g8');
-  addPiece('king', 'white', 'e1');
-  addPiece('king', 'black', 'e8');
-  addPiece('bishop', 'white', 'c1');
-  addPiece('bishop', 'white', 'f1');
-  addPiece('bishop', 'black', 'c8');
-  addPiece('bishop', 'black', 'f8');
-  addPiece('queen', 'white', 'd1');
-  addPiece('queen', 'black', 'd8');
-}
-function selectPiece(){
-  let square = $(event.target).parent()[0];
-  if ($('.active').length > 0 && ($('.active').children().data().team != $(event.target).data().team)){
-    movePiece($('.active')[0].id ,square.id);
-    return;
-  }
-  if($(square).hasClass('active')){
-    $('.active').removeClass('active');
-    return;
-  }
-  $('.active').removeClass('active');
-  $(square).addClass('active');
-}
-function selectNewLocation(){
-  let squareClicked = event.path.reverse()[5];
-  if ($(squareClicked).children().length === 0  && $('.active').length > 0){
-    movePiece($('.active').attr('id'), squareClicked.id)
-  }
-}
-function getGameState(){
-  let board = [];
-  let currentRow = [];
-  let numberOfSquares = $('.square').length;
-  for (let i =0; i<numberOfSquares;i++){
-    let currentSquare = $('.square')[i];
-    if($(currentSquare).children().length === 0){
-      currentRow.push('');
-    }else{
-      let type;
-      let thePiece = $(currentSquare).children();
-      if ($(thePiece).data().piece === 'knight'){
-        type = 'n';
-      }else{
-        type = $(thePiece).data().piece[0];
-      }
-      let color = $(thePiece).data().team[0];
-      currentRow.push(`${color}${type}`);
-      // [color, type]
-    }
-    if(currentRow.length === 8){
-      board.push(currentRow);
-      currentRow = [];
-    }
-  }
-  return board;
-}
 function setGameState(state){
   if (validateGameState(state)){
     clearBoard();
-    console.log('das some good data');
+    // console.log('das some good data');
     let numberOfSquares = $('.square').length
     let squareIndex = 0;
     for (let row = 0; row< 8; row++){
@@ -146,6 +116,28 @@ function validateGameState(state){
 }
 function clearBoard(){
   $('.square').empty();
+}
+function generateBoard(){
+  const $body = $('body');
+  let alphaIndex = ['a','b','c','d','e','f','g','h'];
+  let color;
+  for(let ni=8; ni>=1;ni--){
+
+    for(let ai = 0; ai<8;ai++){
+
+      if(ni%2==0&&ai%2==0){
+        color = 'light';
+      }else if (ni%2==1&&ai%2==1){
+        color = 'light';
+      }else{
+        color = 'dark'
+      }
+
+
+      $('#boardZone').append(`<div class="square ${color}" id="${alphaIndex[ai]}${ni}"></div>`);
+    }
+
+  }
 }
 function parseShorthand(c, p){
   let color;
@@ -180,3 +172,57 @@ function parseShorthand(c, p){
   }
   return `${color}-${piece}`;
 }
+// deletable functions...
+// function setPieces(){
+//   for(let i=0; i<8;i++){
+//     let letters = ['a','b','c','d','e','f','g','h'];
+//     addPiece('pawn','black',`${letters[i]}7`);
+//     addPiece('pawn','white',`${letters[i]}2`);
+//   }
+//   addPiece('rook', 'white', 'a1');
+//   addPiece('rook', 'white', 'h1');
+//   addPiece('rook', 'black', 'a8');
+//   addPiece('rook', 'black', 'h8');
+//   addPiece('knight', 'white', 'b1');
+//   addPiece('knight', 'white', 'g1');
+//   addPiece('knight', 'black', 'b8');
+//   addPiece('knight', 'black', 'g8');
+//   addPiece('king', 'white', 'e1');
+//   addPiece('king', 'black', 'e8');
+//   addPiece('bishop', 'white', 'c1');
+//   addPiece('bishop', 'white', 'f1');
+//   addPiece('bishop', 'black', 'c8');
+//   addPiece('bishop', 'black', 'f8');
+//   addPiece('queen', 'white', 'd1');
+//   addPiece('queen', 'black', 'd8');
+// }
+// function removePiece(location){
+//   $(`#${location}`).html('')
+// }
+// function getGameState(){
+//   let board = [];
+//   let currentRow = [];
+//   let numberOfSquares = $('.square').length;
+//   for (let i =0; i<numberOfSquares;i++){
+//     let currentSquare = $('.square')[i];
+//     if($(currentSquare).children().length === 0){
+//       currentRow.push('');
+//     }else{
+//       let type;
+//       let thePiece = $(currentSquare).children();
+//       if ($(thePiece).data().piece === 'knight'){
+//         type = 'n';
+//       }else{
+//         type = $(thePiece).data().piece[0];
+//       }
+//       let color = $(thePiece).data().team[0];
+//       currentRow.push(`${color}${type}`);
+//       // [color, type]
+//     }
+//     if(currentRow.length === 8){
+//       board.push(currentRow);
+//       currentRow = [];
+//     }
+//   }
+//   return board;
+// }
